119c70e3601b86efec676891260ceb222b8bedb0
ubaidfaridh <57377840+ubaidfaridh@users.noreply.github.com>
2021-11-06 15:18:50 +0700

    update home black

--- project_uas_flutter/lib/screens/home.dart
+++ project_uas_flutter/lib/screens/home.dart
@@ -25,7 +25,7 @@ class _HomeState extends State<Home> {
               ),
               IconButton(
                 onPressed: () {}, 
-                icon: Icon(Icons.home, color: Colors.white)
+                icon: Icon(Icons.home, color: Colors.black)
               ),
               
             ],
